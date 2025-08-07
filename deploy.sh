c3a9768346b52b7b589925e51de1b60d30967462
Noah Celuch <noah.celuch@gmail.com>
2025-08-06 20:55:54 -0400

    ci: add environment variable setup

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,16 @@
 cd go-service; ./build.sh; cd ..
 
 cd infra; terraform plan; terraform apply -auto-approve
+
+api_gateway_url=$(terraform output api_gateway_url)
+auth0_client_id=$(terraform output auth0_client_id)
+
+cd ..; cd client
+
+sed -i '' "s/VITE_AUTH0_CLIENT_ID=.*/VITE_AUTH0_CLIENT_ID=$auth0_client_id/" .env
+sed -i '' "s|VITE_AUDIENCE=.*|VITE_AUDIENCE=$api_gateway_url|" .env
+
+yarn dev &
+
+sleep 5
+open -a firefox -g http://localhost:5173/login
--- teardown.sh
+++ teardown.sh
@@ -1,3 +1,5 @@
+killall node
+
 cd infra; terraform destroy -auto-approve; rm -f authorizer.zip service_1.zip service_2.zip; cd ..
 
 cd go-service; rm -f bootstrap; cd ..
